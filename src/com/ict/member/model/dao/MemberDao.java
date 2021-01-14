42d88a266471985fab261a574e6047dd15483e22
seungje-yu <coffit23@gmail.com>
2021-01-14 16:53:18 +0900

    2021-01-14 insertMember method

--- src/com/ict/member/model/dao/MemberDao.java
+++ src/com/ict/member/model/dao/MemberDao.java
@@ -1,5 +1,14 @@
 package com.ict.member.model.dao;
 
+import org.apache.ibatis.session.SqlSession;
+
+import com.ict.member.model.vo.Member;
+
 public class MemberDao {
+
+	public int insertMember(SqlSession session, Member mem) {
+
+		return 0;
+	}
 
 }
